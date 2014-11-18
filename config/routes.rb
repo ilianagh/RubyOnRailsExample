c86f9e7609f15200077ad44ab47af774f9873220
ilianagh <ghiliana@gmail.com>
2014-11-17 23:57:22 -0600

    spanish

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/info'
 
-  root :to => redirect('/gifts')
+  # root :to => redirect('/gifts')
+  root 'gifts#index'
   resources :gifts
 
   # The priority is based upon order of creation: first created -> highest priority.
